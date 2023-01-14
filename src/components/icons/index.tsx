e17664c4b4966730f6bc360f05e35f6e3ef05e83
fransfilastap@live.com <fransfilastap@Live.com>
2023-01-14 22:51:32 +0700

    feat: more icons

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -71,3 +71,95 @@ export const SystemIcon: FunctionComponent<IconProps> = ({
     </svg>
   );
 };
+
+export const SearchIcon: FunctionComponent<IconProps> = ({
+  className,
+  ...props
+}: IconProps) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      className={clsxtw('w-6 h-6', className)}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
+      />
+    </svg>
+  );
+};
+
+export const ArrowLongIcon: FunctionComponent<IconProps> = ({
+  className,
+  ...props
+}: IconProps) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      className={clsxtw('w-6 h-6', className)}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3'
+      />
+    </svg>
+  );
+};
+
+export const Bars2Icon: FunctionComponent<IconProps> = ({
+  className,
+  ...props
+}: IconProps) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      className={clsxtw('w-6 h-6', className)}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M3.75 9h16.5m-16.5 6.75h16.5'
+      />
+    </svg>
+  );
+};
+
+export const ChevronDownIcon: FunctionComponent<IconProps> = ({
+  className,
+  ...props
+}: IconProps) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      className={clsxtw('w-6 h-6', className)}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M19.5 8.25l-7.5 7.5-7.5-7.5'
+      />
+    </svg>
+  );
+};
