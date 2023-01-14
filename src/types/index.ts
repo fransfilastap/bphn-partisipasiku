85b3131645a1be535fa6de45918ead28b8abfcfc
fransfilastap@live.com <fransfilastap@Live.com>
2023-01-14 22:44:00 +0700

    refactor: organize types by domain

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,23 +1,4 @@
-export interface IAppInfo {
-  siteName: string;
-  siteDescription: string;
-  url: string;
-  email: string;
-  twitter: string;
-  keywords: string[];
-}
+import { ColorMode, IAppInfo, ISeo } from '@/types/common';
+import { Issue, IssueCategory } from '@/types/model';
 
-export interface ISeo {
-  title: string;
-  siteName: string;
-  description: string;
-  url: string;
-  type: string;
-  image: string;
-  locale: string;
-  robots: string;
-  twitterHandler: string;
-  cardType: string;
-}
-
-export type ColorMode = 'light' | 'dark' | 'system';
+export type { ColorMode, IAppInfo, ISeo, Issue, IssueCategory };
---
+++ src/types/model.ts
@@ -0,0 +1,16 @@
+export type Issue = {
+  id: number;
+  title: string;
+  description: string;
+  slug: string;
+  cover: string;
+  placeholder: string;
+  author: string;
+};
+
+export type IssueCategory = {
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+};
