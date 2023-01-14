cd08f419c38e24de0fe542b0a734e878f60a3ef5
fransfilastap@live.com <fransfilastap@Live.com>
2023-01-14 23:15:47 +0700

    refactor: change image quality

--- src/server/mocks/issue.ts
+++ src/server/mocks/issue.ts
@@ -9,7 +9,7 @@ export const mockFetchIssues = (): Issue[] => {
       id: 1,
       author: 'Kementerian A',
       cover:
-        'https://images.unsplash.com/photo-1671748274551-3221f157dd64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80',
+        'https://images.unsplash.com/photo-1671748274551-3221f157dd64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=30',
       title: 'Naskah Akademik ABC',
       description: 'Lorem ipsum dolor sit amet',
       placeholder: '',
@@ -19,7 +19,7 @@ export const mockFetchIssues = (): Issue[] => {
       id: 2,
       author: 'Kementerian A',
       cover:
-        'https://images.unsplash.com/photo-1671748275662-fb4ba29d6e8d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1105&q=80',
+        'https://images.unsplash.com/photo-1671748275662-fb4ba29d6e8d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1105&q=30',
       title: 'Naskah Akademik ABC',
       description: 'Lorem ipsum dolor sit amet',
       placeholder: '',
@@ -29,7 +29,7 @@ export const mockFetchIssues = (): Issue[] => {
       id: 3,
       author: 'Kementerian A',
       cover:
-        'https://images.unsplash.com/photo-1671600493594-edb52f00c6d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1577&q=80',
+        'https://images.unsplash.com/photo-1671600493594-edb52f00c6d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1577&q=30',
       title: 'Naskah Akademik ABC',
       description: 'Lorem ipsum dolor sit amet',
       placeholder: '',
@@ -39,7 +39,7 @@ export const mockFetchIssues = (): Issue[] => {
       id: 3,
       author: 'Kementerian A',
       cover:
-        'https://images.unsplash.com/photo-1669756117671-0642b96141ee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
+        'https://images.unsplash.com/photo-1669756117671-0642b96141ee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=30',
       title: 'Naskah Akademik ABC',
       description: 'Lorem ipsum dolor sit amet',
       placeholder: '',
@@ -49,7 +49,7 @@ export const mockFetchIssues = (): Issue[] => {
       id: 4,
       author: 'Kementerian A',
       cover:
-        'https://images.unsplash.com/photo-1670248012895-1cd8139bf32c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2232&q=80',
+        'https://images.unsplash.com/photo-1670248012895-1cd8139bf32c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2232&q=30',
       title: 'Naskah Akademik ABC',
       description: 'Lorem ipsum dolor sit amet',
       placeholder: '',
@@ -59,7 +59,7 @@ export const mockFetchIssues = (): Issue[] => {
       id: 5,
       author: 'Kementerian A',
       cover:
-        'https://images.unsplash.com/photo-1672561842799-35800adeb44d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1037&q=80',
+        'https://images.unsplash.com/photo-1672561842799-35800adeb44d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1037&q=30',
       title: 'Naskah Akademik ABC',
       description: 'Lorem ipsum dolor sit amet',
       placeholder: '',
@@ -69,7 +69,7 @@ export const mockFetchIssues = (): Issue[] => {
       id: 6,
       author: 'Kementerian A',
       cover:
-        'https://images.unsplash.com/photo-1672312015469-459015b5b97d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80',
+        'https://images.unsplash.com/photo-1672312015469-459015b5b97d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=30',
       title: 'Naskah Akademik ABC',
       description: 'Lorem ipsum dolor sit amet',
       placeholder: '',
