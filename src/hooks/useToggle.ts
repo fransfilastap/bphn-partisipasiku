5c087b5600b0cca5855d52ed9d21ee9feb05acbc
fransfilastap@live.com <fransfilastap@Live.com>
2023-01-14 23:30:02 +0700

    style: disable explisit any

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,14 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Reducer, useReducer } from 'react';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const toggleReducer = (state: boolean, nextValue?: any) =>
   typeof nextValue === 'boolean' ? nextValue : !state;
 
-// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-explicit-any
 const useToggle = (
   initialValue: boolean
 ): [boolean, (nextValue?: any) => void] => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return useReducer<Reducer<boolean, any>>(toggleReducer, initialValue);
 };
 
